cb0aa8184e5a31224fb692e746241c4fea8c0077
Jagadish <Jagadish@192.168.1.46>
2025-01-08 21:12:19 +0530

    Add a New editEmploymentStatus class.

---
+++ src/test/java/com/Hrms/tests/NeweditEmploymentStatus.java
@@ -0,0 +1,12 @@
+package com.Hrms.tests;
+
+public class NeweditEmploymentStatus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String G ="Welcome to GitHub";
+		System.out.println("G");
+		
+	}
+
+}
